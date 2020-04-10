539fab858a6ebbecc4bd49e0ad94056dd18f0d4a
代胜亮 <1596747222@qq.com>
2020-04-10 14:24:08 +0800

    paimai

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import { Button, Select,carousel,CarouselItem,Pagination,Tabs,TabPane,Table,
+  TableColumn,Dialog,InputNumber} from 'element-ui';
+Vue.use(Button).use(Select).use(carousel).use(CarouselItem).use(Pagination).use(Tabs).use(TabPane).use(Table).use(TableColumn).use(Dialog).use(InputNumber)
 Vue.config.productionTip = false
-
+import '@/assets/css/reset.min.css'
 new Vue({
   router,
   store,
